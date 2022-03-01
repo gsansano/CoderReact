46fd670dd76d0d58b87124d818ddc332ddc9c3be
Guillermo Sansano <guillermosansano@gmail.com>
2022-03-01 17:06:28 -0300

    Agregado contador de productos ItemCount

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import './App.css';
+import ItemCount from './components/ItemCount';
 import CartWidget from './components/NavBar/CartWidget';
 import NavBar from './components/NavBar/NavBar';
 
 function App() {
   const greeting = () => NavBar.appendChild(ItemListContainer)
+  const onAdd = (cant) => {
+    console.log(cant) 
+ }
 
   function ItemListContainer() {
     
     return (
       <div id="saludo">Bienvenido a la tienda online del Estudio</div>
     )
+    
   }
 
   return (
@@ -18,6 +23,7 @@ function App() {
       <CartWidget /> 
       </NavBar>
     <ItemListContainer greeting={greeting} />
+    <ItemCount initial={1} stock= {10} onAdd= {onAdd} />
     </div>
   );
 }
